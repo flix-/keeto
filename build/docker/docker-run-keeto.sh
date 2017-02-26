7634895d3f6764951783b42d15993c80c8a320c3
sebastian roland <seroland86@gmail.com>
2017-02-26 20:47:56 +0100

    switch to docker compose

--- build/docker/docker-run-keeto.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-NETWORK_NAME="keeto-net"
-SUBNET="192.168.86.0/29"
-
-docker network create --subnet ${SUBNET} ${NETWORK_NAME}
-./keeto-openldap/docker-run-openldap.sh
-./keeto-openssh/docker-run-openssh.sh
-
