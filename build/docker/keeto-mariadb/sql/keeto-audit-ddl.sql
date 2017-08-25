3f8d64dacafba080b2efd877bf331094fa8ca055
Sebastian Roland <seroland86@gmail.com>
2017-08-25 22:32:15 +0200

    backup - setup audit logging

---
+++ build/docker/keeto-mariadb/sql/keeto-audit-ddl.sql
@@ -0,0 +1,30 @@
+CREATE TABLE openssh_connect (
+session_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+timestamp DATETIME NOT NULL,
+server_addr TEXT NOT NULL,
+client_addr TEXT NOT NULL,
+client_port MEDIUMINT NOT NULL
+);
+
+CREATE TABLE openssh_fingerprint (
+hash_algo ENUM('MD5', 'SHA256'),
+fingerprint TEXT NOT NULL,
+username TEXT NOT NULL
+);
+
+CREATE TABLE openssh_auth (
+session_id BIGINT UNSIGNED NOT NULL,
+timestamp DATETIME NOT NULL,
+event ENUM('SSH_AUTH_FAILURE', 'SSH_AUTH_SUCCESS'),
+username TEXT NOT NULL,
+hash_algo ENUM('MD5', 'SHA256'),
+fingerprint TEXT NOT NULL,
+CONSTRAINT FOREIGN KEY(session_id) REFERENCES openssh_connect(session_id)
+);
+
+CREATE TABLE openssh_disconnect (
+session_id BIGINT UNSIGNED NOT NULL,
+timestamp DATETIME NOT NULL,
+CONSTRAINT FOREIGN KEY(session_id) REFERENCES openssh_connect(session_id)
+);
+
