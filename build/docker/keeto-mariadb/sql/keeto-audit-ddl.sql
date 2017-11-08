317aa69f7593071572cca247e0685e4b617c56ba
Sebastian Roland <seroland86@gmail.com>
2017-11-08 07:40:13 +0100

    utc in database...

--- build/docker/keeto-mariadb/sql/keeto-audit-ddl.sql
+++ build/docker/keeto-mariadb/sql/keeto-audit-ddl.sql
@@ -1,24 +1,24 @@
 CREATE TABLE openssh_connect (
 session_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 timestamp DATETIME NOT NULL,
-server_addr TEXT NOT NULL,
-client_addr TEXT NOT NULL,
-client_port MEDIUMINT NOT NULL
+server_addr TINYTEXT NOT NULL,
+client_addr TINYTEXT NOT NULL,
+client_port SMALLINT UNSIGNED NOT NULL
 );
 
 CREATE TABLE keeto_fingerprint (
-username TEXT NOT NULL,
-hash_algo ENUM('MD5', 'SHA256'),
-fingerprint TEXT NOT NULL
+username TINYTEXT NOT NULL,
+hash_algo ENUM('MD5', 'SHA256') NOT NULL,
+fingerprint TINYTEXT NOT NULL
 );
 
 CREATE TABLE openssh_auth (
 session_id BIGINT UNSIGNED NOT NULL,
 timestamp DATETIME NOT NULL,
-event ENUM('OPENSSH_AUTH_FAILURE', 'OPENSSH_AUTH_SUCCESS'),
-username TEXT NOT NULL,
-hash_algo ENUM('MD5', 'SHA256'),
-fingerprint TEXT NOT NULL,
+event ENUM('OPENSSH_AUTH_FAILURE', 'OPENSSH_AUTH_SUCCESS') NOT NULL,
+username TINYTEXT NOT NULL,
+hash_algo ENUM('MD5', 'SHA256') NOT NULL,
+fingerprint TINYTEXT NOT NULL,
 CONSTRAINT FOREIGN KEY(session_id) REFERENCES openssh_connect(session_id)
 );
 
