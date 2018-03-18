adefcfa258411afd64ed90cda82f5d1d00c201ea
Sebastian Roland <seroland86@gmail.com>
2018-03-18 21:15:45 +0100

    finalize docker audit logging

--- build/docker/keeto-mariadb/sql/keeto-audit-ddl.sql
+++ build/docker/keeto-mariadb/sql/keeto-audit-ddl.sql
@@ -1,30 +1,35 @@
 CREATE TABLE openssh_connect (
-session_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+session_id BIGINT UNSIGNED NOT NULL,
 timestamp DATETIME NOT NULL,
-server_addr TINYTEXT NOT NULL,
-client_addr TINYTEXT NOT NULL,
+server_addr VARCHAR(255) NOT NULL,
+client_addr VARCHAR(255) NOT NULL,
 client_port SMALLINT UNSIGNED NOT NULL
 );
+ALTER TABLE openssh_connect ADD CONSTRAINT pk_openssh_connect PRIMARY KEY (session_id);
+ALTER TABLE openssh_connect MODIFY session_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT;
 
 CREATE TABLE keeto_fingerprint (
-username TINYTEXT NOT NULL,
+username VARCHAR(32) NOT NULL,
 hash_algo ENUM('MD5', 'SHA256') NOT NULL,
-fingerprint TINYTEXT NOT NULL
+fingerprint VARCHAR(255) NOT NULL
 );
+ALTER TABLE keeto_fingerprint ADD CONSTRAINT pk_keeto_fingerprint PRIMARY KEY (username, hash_algo, fingerprint);
 
 CREATE TABLE openssh_auth (
 session_id BIGINT UNSIGNED NOT NULL,
 timestamp DATETIME NOT NULL,
 event ENUM('OPENSSH_AUTH_FAILURE', 'OPENSSH_AUTH_SUCCESS') NOT NULL,
-username TINYTEXT NOT NULL,
+username VARCHAR(32) NOT NULL,
 hash_algo ENUM('MD5', 'SHA256') NOT NULL,
-fingerprint TINYTEXT NOT NULL,
-CONSTRAINT FOREIGN KEY(session_id) REFERENCES openssh_connect(session_id)
+fingerprint VARCHAR(255) NOT NULL
 );
+ALTER TABLE openssh_auth ADD CONSTRAINT pk_openssh_auth PRIMARY KEY (session_id, timestamp, event, username, hash_algo, fingerprint);
+ALTER TABLE openssh_auth ADD CONSTRAINT fk_openssh_auth_openssh_connect FOREIGN KEY (session_id) REFERENCES openssh_connect (session_id);
 
 CREATE TABLE openssh_disconnect (
 session_id BIGINT UNSIGNED NOT NULL,
-timestamp DATETIME NOT NULL,
-CONSTRAINT FOREIGN KEY(session_id) REFERENCES openssh_connect(session_id)
+timestamp DATETIME NOT NULL
 );
+ALTER TABLE openssh_disconnect ADD CONSTRAINT pk_openssh_disconnect PRIMARY KEY (session_id, timestamp);
+ALTER TABLE openssh_disconnect ADD CONSTRAINT fk_openssh_disconnect_openssh_connect FOREIGN KEY (session_id) REFERENCES openssh_connect (session_id);
 
