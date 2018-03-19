1611e3bdd50e32f25be92f9eadf4b5fa41fab822
Sebastian Roland <seroland86@gmail.com>
2018-03-19 20:53:08 +0100

    add openssh server port to audit db / change pk of fingerprint table

--- build/docker/keeto-mariadb/sql/keeto-audit-ddl.sql
+++ build/docker/keeto-mariadb/sql/keeto-audit-ddl.sql
@@ -2,6 +2,7 @@ CREATE TABLE openssh_connect (
 session_id BIGINT UNSIGNED NOT NULL,
 timestamp DATETIME NOT NULL,
 server_addr VARCHAR(255) NOT NULL,
+server_port SMALLINT UNSIGNED NOT NULL,
 client_addr VARCHAR(255) NOT NULL,
 client_port SMALLINT UNSIGNED NOT NULL
 );
@@ -13,7 +14,7 @@ username VARCHAR(32) NOT NULL,
 hash_algo ENUM('MD5', 'SHA256') NOT NULL,
 fingerprint VARCHAR(255) NOT NULL
 );
-ALTER TABLE keeto_fingerprint ADD CONSTRAINT pk_keeto_fingerprint PRIMARY KEY (username, hash_algo, fingerprint);
+ALTER TABLE keeto_fingerprint ADD CONSTRAINT pk_keeto_fingerprint PRIMARY KEY (hash_algo, fingerprint);
 
 CREATE TABLE openssh_auth (
 session_id BIGINT UNSIGNED NOT NULL,
